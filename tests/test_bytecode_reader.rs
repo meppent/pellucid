514d7086c1ea944ba363982f5ca5f8a9b4fe2672
makcandrov <makcandrov@proton.me>
2022-08-29 06:42:15 -0700

    test last push

--- tests/test_bytecode_reader.rs
+++ tests/test_bytecode_reader.rs
@@ -1,30 +1,29 @@
 use hex;
 use pellucid::bytecode_reader::bytecode;
+use pellucid::bytecode_reader::opcode;
+use pellucid::bytecode_reader::vopcode;
+use primitive_types::U256;
 use rand;
 mod utils;
 
 #[test]
-#[should_panic]
-fn test_odd_size_bytecode1() {
-    bytecode::Bytecode::from("abc");
-}
-
-#[test]
-#[should_panic]
-fn test_odd_size_bytecode2() {
-    bytecode::Bytecode::from("0xabc");
-}
-
-#[test]
-#[should_panic]
-fn test_invalid_character_in_bytecode1() {
-    bytecode::Bytecode::from("abcg");
-}
-
-#[test]
-#[should_panic]
-fn test_invalid_character_in_bytecode2() {
-    bytecode::Bytecode::from("0xabcg");
+fn test_invalid_bytecode() {
+    assert!(
+        std::panic::catch_unwind(|| bytecode::Bytecode::from("abc")).is_err(),
+        "Odd size bytecode without 0x did not panic"
+    );
+    assert!(
+        std::panic::catch_unwind(|| bytecode::Bytecode::from("0xabc")).is_err(),
+        "Odd size bytecode with 0x did not panic"
+    );
+    assert!(
+        std::panic::catch_unwind(|| bytecode::Bytecode::from("abcg")).is_err(),
+        "Bytecode with invalid character without 0x did not panic"
+    );
+    assert!(
+        std::panic::catch_unwind(|| bytecode::Bytecode::from("0xabcg")).is_err(),
+        "Bytecode with invalid character with 0x did not panic"
+    );
 }
 
 #[test]
@@ -39,5 +38,40 @@ fn test_0x_support() {
     assert_eq!(
         bytecode::Bytecode::from(&bytecode1),
         bytecode::Bytecode::from(&bytecode2)
+    );
+}
+
+#[test]
+fn test_last_push() {
+    let bytecode = bytecode::Bytecode::from("6001");
+    let last_vopcode = bytecode.get_vopcode_at(bytecode.get_last_pc());
+    assert!(
+        last_vopcode.is_last &&
+        last_vopcode.opcode == opcode::Opcode::PUSH1 &&
+        last_vopcode.value == Some(U256::from(1))
+    );
+
+    let bytecode = bytecode::Bytecode::from("60");
+    let last_vopcode = bytecode.get_vopcode_at(bytecode.get_last_pc());
+    assert!(
+        last_vopcode.is_last &&
+        last_vopcode.opcode == opcode::Opcode::PUSH1 &&
+        last_vopcode.value == None
+    );
+
+    let bytecode = bytecode::Bytecode::from("000065000000000002");
+    let last_vopcode = bytecode.get_vopcode_at(bytecode.get_last_pc());
+    assert!(
+        last_vopcode.is_last &&
+        last_vopcode.opcode == opcode::Opcode::PUSH6 &&
+        last_vopcode.value == Some(U256::from(2))
+    );
+
+    let bytecode = bytecode::Bytecode::from("0000650000000002");
+    let last_vopcode = bytecode.get_vopcode_at(bytecode.get_last_pc());
+    assert!(
+        last_vopcode.is_last &&
+        last_vopcode.opcode == opcode::Opcode::PUSH6 &&
+        last_vopcode.value == None
     );
 }
