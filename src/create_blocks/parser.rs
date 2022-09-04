b68a3414c24ff8f454b8a3b12f8290284fbc9f81
makcandrov <makcandrov@proton.me>
2022-09-04 05:07:11 -0700

    debug current vopcode

--- src/create_blocks/parser.rs
+++ src/create_blocks/parser.rs
@@ -17,9 +17,9 @@ fn find_blocks<'a>(bytecode: &'a Bytecode) -> HashMap<usize, Block<'a>> {
         let mut current_vopcode = vopcode_start;
         let symbolic_block: RefCell<SymbolicBlock> = RefCell::new(SymbolicBlock::new());
 
-        let mut insert_block = || {
+        let mut insert_block = |pc_end: usize| {
             let pc_start = vopcode_start.pc;
-            let mut block_to_add = Block::new(bytecode.slice_code(pc_start, current_vopcode.pc));
+            let mut block_to_add = Block::new(bytecode.slice_code(pc_start, pc_end));
             block_to_add.attach_symbolic_block(symbolic_block.take());
             blocks.insert(
                 pc_start,
@@ -34,18 +34,18 @@ fn find_blocks<'a>(bytecode: &'a Bytecode) -> HashMap<usize, Block<'a>> {
 
             // It's the end of the block, and there is no block after
             if vopcode_iterator.peek() == None || current_vopcode.opcode.is_exiting() || current_vopcode.opcode == Opcode::JUMP {
-                insert_block();
+                insert_block(current_vopcode.pc);
                 break 'same_block;
             // It's the end of the block, and there is a new block after
             } else if current_vopcode.opcode == Opcode::JUMPI {
-                insert_block();
+                insert_block(current_vopcode.pc);
                 continue 'new_block;
             } 
 
             match vopcode_iterator.peek() {
                 // It's the end of the block, and there is a new block after
                 Some(next_vopcode) if next_vopcode.opcode == Opcode::JUMPDEST => {
-                    insert_block();
+                    insert_block(current_vopcode.pc);
                     continue 'new_block;
                 },
                 _ => (),
